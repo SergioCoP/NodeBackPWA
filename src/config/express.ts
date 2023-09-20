81f0c67327fe7868491ed88108bc0b5eb5e907a8
20203tn125 <20203tn125@utez.edu.mx>
2023-09-19 18:58:11 -0600

    [ADD] Configuracion express y postgress

---
+++ src/config/express.ts
@@ -0,0 +1,18 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+const cors = require('cors');
+
+const app = express();
+app.use(
+    cors({
+        origin: '*'
+    })
+);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json({limit:'20mb'})); //limite de mb por peticion
+app.get('/',(req,res)=>{
+    res.send('Server running...');
+});
+
+export default app;
