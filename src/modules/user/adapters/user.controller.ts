52195f25e0d04addcbb6bccfb918aeb89fe7246e
20203tn125 <20203tn125@utez.edu.mx>
2023-09-25 19:00:36 -0600

    [UPDATE] Avance servicio

--- src/modules/user/adapters/user.controller.ts
+++ src/modules/user/adapters/user.controller.ts
@@ -1,1 +1,21 @@
+import express,{Request, Response} from 'express'
+import { UserStorageGateway } from './user.storage.gateway';
+import { GetAllUsersInteractor } from '../use-cases/get-all-users.interactor';
 //Controller (Spring)
+export class UserController {
+
+    static async getAll (req: Request,res:Response) {
+        try {
+              const repository = new UserStorageGateway();
+              const interactor = new GetAllUsersInteractor(repository);
+              const data = await interactor.execute();
+              res.status(200).json(data);
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({message:'Error'})
+        }
+    }
+}
+
+export const userRouter = express.Router();
+userRouter.get('/',[],UserController.getAll);
